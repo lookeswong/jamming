22481d3b6f0935a885ce2c85e43007d5c851ee82
LooKes Wong <lookeswong@gmail.com>
2022-10-27 01:13:12 +0100

    Hook up search bar to Spotify Search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -47,13 +48,17 @@ class App extends React.Component {
     const trackURIs = this.state.playlistTracks.map(track => track.uri);
   }
 
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
           {/* <!-- Add a SearchBar component --> */}
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             {/* <!-- Add a SearchResults component --> */}
             <SearchResults searchResults={this.state.SearchResults} onAdd={this.addTrack}/>
