e82cb93029d8f11d3b31c2554961ba3baa8dd93d
LooKes Wong <lookeswong@gmail.com>
2022-10-28 15:37:31 +0100

    Implement Spotify search request

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+import SearchBar from "../Components/SearchBar/SearchBar";
+
 const clientID = "304dcb333920440a9cc900e8668591bf";
 const redirectURI = "http://localhost:3000/";
 let accessToken;
@@ -23,7 +25,30 @@ const Spotify = {
       const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
       window.location = accessUrl;
     }
-  }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { 
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return []
+      }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
+    });
+  },
+
 }
 
 
