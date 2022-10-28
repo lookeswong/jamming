5d92a09879f5239c7005100e3e45abcbc2b13fff
LooKes Wong <lookeswong@gmail.com>
2022-10-28 15:22:23 +0100

    Set up Spotify access token

---
+++ src/util/Spotify.js
@@ -0,0 +1,30 @@
+const clientID = "304dcb333920440a9cc900e8668591bf";
+const redirectURI = "http://localhost:3000/";
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    // check for access token match
+    const accessTokenMatch = window.location.href.match(/accessToken=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresInMatch[1]);
+      // clear the parameters to grabb a new access token when it expires
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessUrl;
+    }
+  }
+}
+
+
+export default Spotify;
